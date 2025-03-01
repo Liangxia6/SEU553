efe5f3e0b2c26a5f78db08e13e04e593147b4461
Liangxia <1187692686@qq.com>
2025-03-02 01:17:29 +0800

    on 3.1 by lx

---
+++ 2019/a_1.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 4, c = 0, b = 1;
+    for (int i = 0; i < 5; i++)
+    {
+        switch ((--a) > 0)
+        {
+        case 0:
+            switch (c++)
+            {
+            case 0:
+                cout << "%";
+            case 1:
+                cout << "#";
+            }
+            break;
+        case 1:
+            switch (b)
+            {
+            case 0:
+                cout << "*";
+                --b;
+                break;
+            case 1:
+                cout << "@";
+                --b;
+                break;
+            }
+        default:
+            cout << "&";
+        }
+        cout << "!" << endl;
+    }
+    return 0;
+}
+
+//@
